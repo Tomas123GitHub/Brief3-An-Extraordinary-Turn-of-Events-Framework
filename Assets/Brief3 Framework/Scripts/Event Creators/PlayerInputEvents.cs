2fa539b315cf4b376fc222f9c38fb185d2f15840
Gareth Lockett <g.lockett@sae.edu>
2020-03-30 16:21:29 +0800

    Major update
    Added new Goal model.
    Added more event/handler/utility scripts.
    Added SciFi materials.
    Added Space Skybox.

---
+++ Assets/Brief3 Framework/Scripts/Event Creators/PlayerInputEvents.cs
@@ -0,0 +1,45 @@
+﻿
+using UnityEngine;
+using UnityEngine.Events;
+
+/*
+    Script: PlayerInputEvents
+    Author: Gareth Lockett
+    Version: 1.0
+    Description:    Simple script for invoking a UnityEvent when a key is pressed on the keyboard.
+*/
+
+public class PlayerInputEvents : MonoBehaviour
+{
+    // Events
+    public UnityEvent keyPressed;                   // Invoked whenever the action key is pressed.
+
+    // Properties
+    public KeyCode key = KeyCode.Space;             // The key to press to invoke the 'key pressed' event.
+    public bool singlePress = true;                 // If this is set to true, then will only invoke events when the key is first pressed. Otherwise will invoke each Update/frame the key is held down on.
+
+    // Methods
+    private void Update()
+    {
+        // Check for key pressed. If using singlePress then check with Input.GetKeyDown() otherwise check with Input.GetKey() (See unity Scripting Reference for more on these)
+        bool keyIsPressed = false;
+        if( this.singlePress == true )
+        {
+            if( Input.GetKeyDown( this.key ) == true ) { keyIsPressed = true; } // Check if the key has been pressed since last Update.
+        }
+        else
+        {
+            if( Input.GetKey( this.key ) == true ) { keyIsPressed = true; } // Check if the key is currently pressed (Eg held down)
+        }
+
+        // If key is pressed, check to invoke event.
+        if( keyIsPressed == true )
+        {
+            if( this.keyPressed != null )    // Check some methods have subscribed to the leftKeyPressed event.
+            {
+                this.keyPressed.Invoke();    // Invoke any methods that have subscribed to the leftKeyPressed event.
+            }
+        }
+
+    }
+}
---
+++ Assets/Brief3 Framework/Scripts/Utilities/EndGoal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    Script: EndGoal
+    Author: Gareth Lockett
+    Version: 1.0
+    Description:    ...
+*/
+
+public class EndGoal : MonoBehaviour
+{
+    // Properties
+    public GameObject ball;                 // Reference to the ball game object.
+
+    // Methods
+    private void OnTriggerEnter( Collider other )
+    {
+        if( other.gameObject == this.ball )
+        {
+            Debug.Log( "Ball in end goal!" );
+        }
+    }
+}
